cc8b2c0e89c66bfed0e1dcb12fbefae91aed4c5c
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2020-03-13 23:46:16 +0200

    fix docker for experiment

--- dockers/experiment/run-experiments.sh
+++ dockers/experiment/run-experiments.sh
@@ -6,20 +6,20 @@ touch endpoints.txt
 
 for i in $(echo $STRABON_ENDPOINTS | sed "s/,/ /g")
 do
-    echo "http://$i.svc.cluster.local:8080/strabon/Query" >> endpoints.txt
+    echo "http://$i.default.svc.cluster.local:8080/strabon/Query" >> endpoints.txt
 done
 
-for i in $(echo SEMAGROW_ENDPOINTS | sed "s/,/ /g")
+for i in $(echo $SEMAGROW_ENDPOINTS | sed "s/,/ /g")
 do
-    echo "http://$i.svc.cluster.local:8080/SemaGrow/sparql" >> endpoints.txt
+    echo "http://$i.default.svc.cluster.local:8080/SemaGrow/sparql" >> endpoints.txt
 done
 
-for i in `seq -w TIMES_TO_RUN`
+for i in `seq -w $TIMES_TO_RUN`
 do
   ./runExperiment.sh endpoints.txt results.$i.csv
 done
 
-for i in `seq -w TIMES_TO_RUN`
+for i in `seq -w $TIMES_TO_RUN`
 do
   echo
   cat results.$i.csv
